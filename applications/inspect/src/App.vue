93871c7ca1ad832579d363171d7dea4b8f8bcaf4
antonmeskildsen <antonmeskildsen@me.com>
2023-09-18 09:30:35 +0200

    Added cdoc-wasm and inspect projects to version control..

---
+++ applications/inspect/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+
+import { RouterLink, RouterView } from 'vue-router'
+import Editor from "@/components/Editor.vue";
+</script>
+
+<template>
+  <header>
+
+
+    <Editor></Editor>
+  </header>
+
+
+</template>
+
---
+++ crates/cdoc-wasm/src/lib.rs
@@ -0,0 +1,23 @@
+mod utils;
+
+use cdoc_parser::ast::Ast;
+use cdoc_parser::document::Document;
+use std::convert::TryFrom;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+extern "C" {
+    fn alert(s: &str);
+}
+
+#[wasm_bindgen]
+pub fn greet() {
+    alert("Hello, cdoc-wasm!");
+}
+
+#[wasm_bindgen]
+pub fn parse_doc(input: &str) -> JsValue {
+    let doc = Document::try_from(input).unwrap();
+
+    serde_wasm_bindgen::to_value(&doc).unwrap()
+}
