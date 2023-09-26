5f9dc84e17b839cfe9239ef4551d623d6d650897
antonmeskildsen <antonmeskildsen@me.com>
2023-09-26 09:29:15 +0200

    Fixed update mechanism when the project configuration is otherwise invalid.

--- src/project/config.rs
+++ src/project/config.rs
@@ -11,6 +11,13 @@ use cdoc::processors::AstPreprocessorConfig;
 use clap::ValueEnum;
 use semver::VersionReq;
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct ProjectConfigDummy {
+    pub courses: CoursesConfig,
+    #[serde(flatten)]
+    pub everything: HashMap<String, serde_yaml::Value>,
+}
+
 /// Refers to a configuration.yml file in the project that specifies a variety
 /// of options for the project.
 #[derive(Debug, Clone, Serialize, Deserialize)]
